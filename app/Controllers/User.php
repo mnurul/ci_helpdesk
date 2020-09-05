999b90e2783d3bf5312a7f069b5e249d414c4ba9
mnurul <mnurulislam05@gmail.com>
2020-09-05 21:26:10 +0700

    update 8 September change view start asking

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -192,6 +192,253 @@ class User extends BaseController
         return view('start_asking/index', $data);
     }
 
+    public function simpanemail()
+    {
+        $getid = $this->input->get('getid');
+        $email = $this->input->get('email');
+        $this->db->query("UPDATE `askpending` SET `email`='" . $email . "' WHERE (`id`='" . $getid . "')");
+    }
+
+    public function caripertanyaan()
+    {
+        echo "test";
+        //-----mengambil data yang di kirim dan mengembalikannya
+        $q = strtolower($_GET["q"]);
+        if (!$q) return;
+
+        //-----Query untuk mencari data yang dikirimkan berdasarkan npm atau nama
+        $sql = "SELECT * FROM vocab WHERE ask LIKE '%" . $q . "%'";
+        $tampil = "SELECT * FROM vocab WHERE ask LIKE '%" . $q . "%'";
+
+        //-----Menampilkan data dari hasil query
+        while ($data = mysqli_fetch_array($tampil)) {
+            echo $data['ask'] . "\n";
+        }
+    }
+
+    public function tanyajawab()
+    {
+        echo "test1";
+        $teks = $this->input->get('tanya');
+        var_dump($teks);
+        $teks = strtolower($teks);
+        $idvocab = $this->session->userdata('idvocab');
+        $ipclient = $this->session->userdata('ipclient');
+        $tesklama = $this->session->userdata('kataterakhir');
+
+        //=====Konfirmasi Benar atau tidaknya sebuah jawaban=====
+        if ($teks == 'ya') {
+            if ($idvocab <> '') {
+                $sql = "SELECT * FROM vocab WHERE id='" . $idvocab . "'";
+                $tampil =  $this->db->query($sql);
+                $data = mysqli_fetch_array($tampil);
+
+                $totalask = $data['tolask'] + 1;
+                $update = "UPDATE `vocab` SET `tolask`='" . $totalask . "' WHERE (`id`='" . $idvocab . "')";
+                $this->db->query($update);
+
+                $data = array('idvocab' => '');
+                $this->session->set_userdata($data);
+
+                echo "Syukur deh kalo sudah bisa.. kalo ada apa-apa lagi silahkan saja hubungi kami kembali ya? terima kasih";
+            } else {
+                echo "Ya, Apakah ada yang bisa kami bantu?";
+            }
+            exit;
+        } elseif ($teks == 'tidak') {
+            if ($idvocab <> '') {
+                $hapus = "DELETE FROM tmpprobabilityvocab WHERE idvocab=" . $idvocab . " AND ipclient='" . $ipclient . "'";
+                $this->db->query($hapus);
+
+                $sql = "SELECT b.id, b.answer FROM tmpprobabilityvocab AS a INNER JOIN vocab AS b ON b.id = a.idvocab WHERE a.ipclient='" . $ipclient . "' ORDER BY a.jumprobability DESC";
+                $tampil = $this->db->query($sql);
+                $data = mysqli_fetch_array($tampil);
+                if (trim($data['answer']) <> '') {
+                    $arr = $data['answer'] . '<br><br>Bagaimana, Bisa tidak? jika bisa ketik `YA` tapi jika tidak bisa ketikkan `TIDAK`, Terima Kasih';
+                    $data = array('idvocab' => $data['id']);
+                    $this->session->set_userdata($data);
+                } else {
+                    $this->db->query("INSERT INTO `askpending` (`ask`, `status`,`tglask`,`ipclient`) VALUES ('" . $tesklama . "', '1', '" . date('Y-m-d H:i:s') . "','" . $ipclient . "')");
+                    $getID = $this->db->insert_id();
+                    $arr = '<p>Maaf untuk sementara ini, pertanyaan yang anda ajukan akan kami diskusikan terlebih dahulu dan akan ditindak lanjuti melalui email apabila sudah ada solusinya.<br /> silahkan masukan email anda <input type="text" name="email' . $getID . '" id="email' . $getID . '">&nbsp;<a onclick="simpanemail(' . $getID . ')" href="#">Kirim</a></p>';
+
+                    $data = array('idvocab' => '');
+                    $this->session->set_userdata($data);
+                }
+                echo $arr;
+            } else {
+                echo "Tidak kenapa? <br> Apakah ada yang bisa kami bantu?";
+            }
+            exit;
+        }
+        //======Exit Konfirmasi Benar atau tidaknya sebuah jawaban=====
+
+
+        if ($teks) {
+            //=====Kamus kata untuk menyamakan atau menyetarakan kata=============
+            $correctword = explode(" ", $teks);
+            $jumdata = count($correctword);
+
+            //-----Pengecekan Kata Minimal Harus Lebih dari 3 kata-----
+            if ($jumdata < 3) {
+                $arr = "Maaf Coba Masukkan Kembali Pertanyaan Anda, karena kami masih tidak paham maksud anda. Terima kasih";
+                echo $arr;
+                exit;
+            }
+            //-----Exit Kata Minimal-----
+
+            $kata = '';
+            for ($i = 0; $i < $jumdata; $i++) {
+                $sql = "SELECT correctword FROM correctword WHERE word='" . $correctword[$i] . "'";
+                $tampil = $this->db->query($sql);
+                $data = mysqli_fetch_array($tampil);
+                if (trim($data['correctword']) <> '') {
+                    $kata .= $data['correctword'] . " ";
+                } else {
+                    $kata .= $correctword[$i] . " ";
+                }
+            }
+            $teks = trim($kata);
+            //=====Exit Kamus Kata===============
+
+            //==========Mencari berdasarkan data yang berkemungkinan terbesar berdasarkan kata=====
+            $this->db->query("DELETE FROM tmpprobabilityvocab WHERE ipclient='" . $ipclient . "'");
+            $correctword = explode(" ", $teks);
+            $jumdata = count($correctword);
+            $kata = '';
+            for ($i = 0; $i < $jumdata; $i++) {
+                $sql = "SELECT * FROM vocab WHERE MATCH(ask) AGAINST('" . $correctword[$i] . "' IN BOOLEAN MODE)";
+                $tampil = $this->db->query($sql);
+                while ($data = mysqli_fetch_array($tampil)) {
+                    //-------------Cek Jumlah Vocab-------------
+                    $sql_jumtmpvocab = "SELECT COUNT(*) jum FROM tmpprobabilityvocab WHERE idvocab=" . $data['id'] . " AND ipclient='" . $ipclient . "'";
+                    $tampil_jumtmpvocab = $this->db->query($sql_jumtmpvocab);
+                    $data_jumtmpvocab = mysqli_fetch_array($tampil_jumtmpvocab);
+                    $jumtmpvocab = $data_jumtmpvocab['jum'];
+                    //-------------Exit Cek Jumlah Vocab--------
+
+                    //-------------Cek Vocab-------------
+                    $sql_tmpvocab = "SELECT * FROM tmpprobabilityvocab WHERE idvocab=" . $data['id'] . " AND ipclient='" . $ipclient . "'";
+                    $tampil_tmpvocab = $this->db->query($sql_tmpvocab);
+                    $data_tmpvocab = mysqli_fetch_array($tampil_tmpvocab);
+                    //-------------Exit Cek Vocab--------
+
+                    if ($jumtmpvocab == 0) {
+                        $simpan = "INSERT INTO `tmpprobabilityvocab` (`idvocab`, `jumprobability`,`ipclient`) VALUES ('" . $data['id'] . "', '1','" . $ipclient . "')";
+                        $this->db->query($simpan);
+                    } else {
+                        $jumprobability = $data_tmpvocab['jumprobability'] + 1;
+                        $update = "UPDATE `tmpprobabilityvocab` SET `jumprobability`=" . $jumprobability . " WHERE (`idvocab`='" . $data['id'] . "') AND (`ipclient`='" . $ipclient . "')";
+                        $this->db->query($update);
+                    }
+                }
+            }
+            //===========Exit Mencara Berdasarkan data berkata==============
+
+
+            //===========Mencari Data Profile Peminta HelpDesk============
+            $sql = "
+				SELECT dept,lokasi,`server`,nama_user FROM komputer WHERE IP='" . $ipclient . "'
+			";
+            $tampil = $this->db->query($sql);
+            while ($data = mysqli_fetch_array($tampil)) {
+                $departement = $data['dept'];
+                $kantor = $data['lokasi'];
+                $server = $data['server'];
+                $pengadu = $data['nama_user'];
+            }
+            //===========Exit Mencari Data Profile Peminta Helpdesk=======
+
+            //===========Memberikan Nilai Bobot Kepada setiap Vocab=======
+            $sql = "
+			SELECT
+				a.id,
+				a.departement,
+				a.kantor,
+				a.`server`,
+				a.pengadu,
+				b.jumprobability
+			FROM
+				vocab AS a
+				LEFT JOIN tmpprobabilityvocab AS b ON b.idvocab = a.id AND b.ipclient = '" . $ipclient . "'
+			";
+            $tampil = $this->db->query($sql);
+            while ($data = mysqli_fetch_array($tampil)) {
+                $bobotdepartement = '0,1';
+                $bobotkantor = '0,2';
+                $bobotserver = '0,3';
+                $bobotpengadu = '0,5';
+
+                $nilaidepartement = '0';
+                $nilaikantor = '0';
+                $nilaiserver = '0';
+                $nilaipengadu = '0';
+
+                if ($data['departement'] == $departement) {
+                    $nilaidepartement = 1;
+                }
+                if ($data['kantor'] == $kantor) {
+                    $nilaikantor = 1;
+                }
+                if ($data['server'] == $server) {
+                    $nilaiserver = 1;
+                }
+                if ($data['pengadu'] == $pengadu) {
+                    $nilaipengadu = 1;
+                }
+
+                $nilai = (($bobotdepartement * $nilaidepartement) + ($bobotkantor * $nilaikantor) + ($bobotserver * $nilaiserver) + ($bobotpengadu + $nilaipengadu) + $data['jumprobability']);
+                $bobot = $bobotdepartement + $bobotkantor + $bobotserver + $bobotpengadu + $jumdata;
+
+                $hasil = $nilai / $bobot;
+
+                //-------------Cek Jumlah Vocab-------------
+                $sql_jumtmpvocab = "SELECT COUNT(*) jum FROM tmpprobabilityvocab WHERE idvocab=" . $data['id'] . " AND ipclient='" . $ipclient . "'";
+                $tampil_jumtmpvocab = $this->db->query($sql_jumtmpvocab);
+                $data_jumtmpvocab = mysqli_fetch_array($tampil_jumtmpvocab);
+                $jumtmpvocab = $data_jumtmpvocab['jum'];
+                //-------------Exit Cek Jumlah Vocab--------
+                if ($hasil == 0) {
+                    $delete = "DELETE FROM `tmpprobabilityvocab` WHERE idvocab=" . $data['id'] . "";
+                } else {
+                    if ($jumtmpvocab == 0) {
+                        $simpan = "INSERT INTO `tmpprobabilityvocab` (`idvocab`, `jumprobability`,`ipclient`) VALUES ('" . $data['id'] . "', '" . $hasil . "','" . $ipclient . "')";
+                        $this->db->query($simpan);
+                    } else {
+                        $update = "UPDATE `tmpprobabilityvocab` SET `jumprobability`=" . $hasil . " WHERE (`idvocab`='" . $data['id'] . "') AND (`ipclient`='" . $ipclient . "')";
+                        $this->db->query($update);
+                    }
+                }
+            }
+            //==========Exit Memberikan Nilai Bobot Kepada Setiap Vocab=====
+
+
+            //===========Pemanggilan Data Yang sudah sesuai atau tidak==========
+            $sql = "SELECT b.id, b.answer FROM tmpprobabilityvocab AS a INNER JOIN vocab AS b ON b.id = a.idvocab WHERE a.ipclient='" . $ipclient . "' ORDER BY a.jumprobability DESC";
+            $tampil = $this->db->query($sql);
+            $data = mysqli_fetch_array($tampil);
+            if (trim($data['answer']) <> '') {
+                //$arr=$data['answer'].'<br><br> Mohon Konfirmasi apakah informasi kami benar atau tidak? jika benar ketik `YA` jika salah ketik `TIDAK`, maka kami akan memberikan informasi lagi yang mungkin... ';
+                $arr = $data['answer'] . '<br><br>Bagaimana, Bisa tidak? jika bisa ketik `YA` tapi jika tidak bisa ketikkan `TIDAK`, Terima Kasih';
+                $data = array('idvocab' => $data['id']);
+                $this->session->set_userdata($data);
+            } else {
+                $this->db->query("INSERT INTO `askpending` (`ask`, `status`,`tglask`,`ipclient`) VALUES ('" . $teks . "', '1', '" . date('Y-m-d H:i:s') . "','" . $ipclient . "')");
+                $getID = $this->db->insert_id();
+                //$arr = '<p>Maaf untuk sementara ini, pertanyaan yang anda ajukan akan kami diskusikan terlebih dahulu dan akan ditindak lanjuti melalui email apabila sudah ada solusinya.<br /> silahkan masukan email anda <input type="text" name="email'.$getID.'" id="email'.$getID.'">&nbsp;<a onclick="simpanemail('.$getID.')" href="#">Kirim</a></p>';	
+                $arr = '<p>Aduh!! Maaf Sepertinya pertanyaan anda tidak ada di dalam databases kami, akan coba kami diskusikan dulu ya kepada team IT, akan kami informasikan kembali apabila sudah. terima kasih</p>
+				';
+            }
+            //============Exit Pemanggilan Data yang sesuai atau tidak=========
+        } else {
+            $arr = 'Kamu mau nanya ap?';
+        }
+        echo $arr;
+
+        $data = array('kataterakhir' => $teks);
+        $this->session->set_userdata($data);
+    }
+
     //--------------------------------------------------------------------
 
 }
--- app/Views/start_asking/index.php
+++ app/Views/start_asking/index.php
@@ -42,14 +42,14 @@
                         <?php }
                         ?>
                         <div class="row">
-                            <div class="column">
+                            <!-- <div class="column">
                                 <form action="" class="form-container" method="POST">
                                     <textarea type="password" id="admininput" name="admininput" class="admininput" placeholder="Chat with Me" autofocus></textarea>
                                     <textarea type="password" id="userinput" name="userinput" class="userinput" placeholder="Chat with My Admin" autofocus></textarea>
                                     <button type="submit" class="btn btn-send">Send</button>
-                                    <!-- <input type="reset" value="Refresh" class="btn btn-refresh"> -->
+                                    <input type="reset" value="Refresh" class="btn btn-refresh">
                                     <button type="reset" class="btn btn-refresh">Refresh</button>
-                                    <!-- <div class="card">
+                                    <div class="card">
                                     <div class="row no-gutters">
                                         <div class="col-6">
                                             <h6 class="card-subtitle st-ticket text-muted"></h6>
@@ -61,8 +61,33 @@
                                     <hr style="margin-top: 10px;">
                                     <h5 class="card-title text"></h5>
                                     <a href="#" class="btn btn-status"></a>
-                                </div> -->
+                                </div>
                                 </form>
+                            </div> -->
+                            <div id="List" class="scrollit">
+                                <table width="800" border="0" align="center">
+                                    <tr>
+                                        <td height="192" width="100%" colspan="2" valign="bottom">
+                                            <table width="100%" border="0" id="tampilchat" cellpadding="2" cellspacing="2">
+                                                <tr>
+                                                    <td valign="top" width="10%">
+
+                                                    </td>
+                                                    <td valign="top" align="">
+                                                        <div id="ConR">
+                                                            <div id="ConRspasi">Ada yang bisa saya bantu?</div>
+                                                        </div>
+                                                    </td>
+                                                    <td width="10%"></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td width="70%"><input required name="tanya" placeholder="Ayo bicara dengan Team IT~!" type="text" id="tanya" size="50" style="width:99%" onKeyUp="inputKeyUp(event)"></td>
+                                        <td width="20%"><input type="button" name="button" onClick="cek()" id="button" value="Kirim" title="Klik tombol untuk mengirim pesan "><input type="button" name="button" id="button" value="Refresh" onclick="refreshbox()" title="Klik tombol untuk merefresh"></td>
+                                    </tr>
+                                </table>
                             </div>
                         </div><br><br>
                     </div>
@@ -73,22 +98,6 @@
 </div>
 </div>
 </div>
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-    (function() {
-        var s1 = document.createElement("script"),
-            s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/5f3cbc7b1e7ade5df4421a6f/default';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
-
 
 <script>
     function myFunction() {
@@ -98,6 +107,54 @@
         } else {
             x.className = "topnav";
         }
+    }
+</script>
+
+<script>
+    function inputKeyUp(e) {
+        e.which = e.which || e.keyCode;
+        if (e.which == 13) {
+            // submit
+            cek();
+        }
+    }
+
+    function cek() {
+        var tanya = document.getElementById('tanya').value;
+        // alert(tanya);
+        var letak = 'right';
+        historichat(tanya, letak)
+        $.post("<?= base_url(); ?>chatsimi/tanyajawab?tanya=" + tanya, $("form").serialize(), function(hasil) {
+            data = hasil.split("|");
+            var letak = '';
+            var kata = data[0];
+            historichatadmin(kata, letak);
+            //$("#jawab").val(data[0]);
+            document.getElementById('tanya').value = '';
+        });
+    }
+
+    function historichat(kata, letak) {
+        $('#tampilchat tr:last').after('<tr><td width="10%"></td><td valign="top" align=\"' + letak + '\"><div id="ConR"><div id="ConRspasi">' + kata + '</div></div></td><td width="10%" valign="top"><h5 style="font-family:Merriweather-700, serif;font-style: normal;font-weight: 700; font-size: 28px;line-height: 34px; color: #244295;">Me</h5></td></tr>');
+    }
+
+    function historichatadmin(kata, letak) {
+        $('#tampilchat tr:last').after('<tr><td valign="top"><h5 style="font-family:Merriweather-700, serif;font-style: normal;font-weight: 700; font-size: 28px;line-height: 34px; color: white;">Admin</h5><img width="85px" height="75px" src="<?php echo base_url(); ?>images/logokiri.png" /></td><td  valign="top" align=\"' + letak + '\"><div id="ConR"><div id="ConRspasi">' + kata + '</div></div></td><td></td></tr>');
+    }
+
+    function refreshbox() {
+        window.open("<?php echo base_url(); ?>/user/start_asking", "_self");
+    }
+
+    function simpanemail(getid) {
+        var email = document.getElementById('email' + getid).value;
+        $.post("<?php echo base_url(); ?>chatsimi/simpanemail?email=" + email + '&getid=' + getid, $("form").serialize(), function(hasil) {
+            data = hasil.split("|");
+            var kata = 'Terima kasih telah menghubungi kami. Apabila ingin berkonsultasi kembali silahkan masukkan pertanyaan anda';
+            var letak = '';
+            historichatadmin(kata, letak);
+
+        });
     }
 </script>
 
